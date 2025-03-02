d943ccde752fde5b29847ac3cc2f2afe642f260a
bytegrn <greenbrightsmart@gmail.com>
2025-03-02 18:56:56 +0100

    handle incoming applink

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { useNavigation } from '@react-navigation/native';
 import * as SplashScreen from 'expo-splash-screen';
 import { useKeepAwake } from 'expo-keep-awake';
+import * as Linking from 'expo-linking';
 import { useEffect } from "react";
 
 import { RootStackNavigation } from "./navigations/RootStackNavigation";
@@ -16,6 +18,17 @@ SplashScreen.setOptions({
 export default function App() {
   const isLoading = useAuthStore(state => state.isLoading);
   useKeepAwake();
+  const navigation = useNavigation();
+
+  const url = Linking.useURL();
+
+  if (url) {
+    const { queryParams } = Linking.parse(url);
+    const includesTransactionRef = JSON.stringify(queryParams).includes('trxref')
+    if (includesTransactionRef) {
+      navigation.navigate('App', { screen: 'Courses' })
+    }
+  }
 
   useEffect(() => {
     // bootstrap app
