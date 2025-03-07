e16ebe9fc479611f35f471937bf91a1c975f607f
bytegrn <greenbrightsmart@gmail.com>
2025-03-07 18:18:37 +0100

    set user subscription to true until update from server

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,10 @@
 import { View, Text, StyleSheet, Image, Dimensions, Pressable, Linking, Alert } from "react-native";
-import Icon from '@expo/vector-icons/MaterialCommunityIcons';
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { useNavigation } from "@react-navigation/native";
-import MenuIcon from '@expo/vector-icons/Ionicons';
+import MenuIcon from "@expo/vector-icons/Ionicons";
 import { useEffect, useState } from "react";
 import { AxiosError } from "axios";
-import Carousel from 'pinar';
+import Carousel from "pinar";
 
 import { useUserSubscriptionStore } from "../stores/subscriptionStore";
 import { colors, screenHorizontalPadding } from "../constants/theme";
@@ -14,206 +14,239 @@ import { useAuthStore } from "../stores/authStore";
 import AvatarView from "../components/avatarView";
 import { globalStyles } from "../styles/global";
 
-
 enum MenuItemScreensRoutes {
-    'COURSES' = 'Courses',
-    'PROJECTS' = 'Projects',
-    'UPLOAD' = 'Upload PQs/Projects',
-    'PREMIUM' = 'Premium',
-    'EARNING' = 'Earning',
-    'SERVICES' = 'Services',
-    'ASK' = 'Ask',
-    'ESCROW_SITE' = 'Transecure Escrow',
+  "COURSES" = "Courses",
+  "PROJECTS" = "Projects",
+  "UPLOAD" = "Upload PQs/Projects",
+  "PREMIUM" = "Premium",
+  "EARNING" = "Earning",
+  "SERVICES" = "Services",
+  "ASK" = "Ask",
+  "ESCROW_SITE" = "Transecure Escrow",
 }
 
 interface IMenuItems {
-    item: MenuItemScreensRoutes,
-    icon: () => React.ReactNode
+  item: MenuItemScreensRoutes;
+  icon: () => React.ReactNode;
 }
 
-
 export default function HomeScreen() {
-    const user = useAuthStore(state => state.user);
-    const navigation = useNavigation();
-    const { data: userData } = user;
-    const { width } = Dimensions.get('window');
-    const slidesPlaceholder = require('../../assets/images/placeholder.png')
+  const user = useAuthStore((state) => state.user);
+  const navigation = useNavigation();
+  const { data: userData } = user;
+  const { width } = Dimensions.get("window");
+  const slidesPlaceholder = require("../../assets/images/placeholder.png");
 
-    const [slidesImages, setSlidesImages] = useState<any[] | []>([]);
+  const [slidesImages, setSlidesImages] = useState<any[] | []>([]);
+  const [isSubscribed, setIsSubscribed] = useState(true);
 
-    const MENU_ITEMS: IMenuItems[] = [
-        {
-            item: MenuItemScreensRoutes.COURSES,
-            icon: () => (<MenuIcon name="book-outline" size={20} color={'#ffffff'} />),
-        },
-        {
-            item: MenuItemScreensRoutes.PROJECTS,
-            icon: () => (<MenuIcon name="folder-open-outline" size={20} color={'#ffffff'} />)
-        },
-        {
-            item: MenuItemScreensRoutes.UPLOAD,
-            icon: () => (<MenuIcon name="cloud-upload-outline" size={20} color={'#ffffff'} />)
-        },
-        {
-            item: MenuItemScreensRoutes.PREMIUM,
-            icon: () => (<MenuIcon name="pricetags-outline" size={20} color={'#ffffff'} />)
-        },
-        {
-            item: MenuItemScreensRoutes.EARNING,
-            icon: () => (<MenuIcon name="cash-outline" size={20} color={'#ffffff'} />)
-        },
-        {
-            item: MenuItemScreensRoutes.SERVICES,
-            icon: () => (<MenuIcon name="briefcase-outline" size={20} color={'#ffffff'} />)
-        },
-        {
-            item: MenuItemScreensRoutes.ASK,
-            icon: () => (<MenuIcon name="chatbubble-ellipses-outline" size={20} color={'#ffffff'} />)
-        },
-        {
-            item: MenuItemScreensRoutes.ESCROW_SITE,
-            icon: () => (<MenuIcon name="shield-checkmark-outline" size={20} color={'#ffffff'} />)
-        },
-    ]
+  const MENU_ITEMS: IMenuItems[] = [
+    {
+      item: MenuItemScreensRoutes.COURSES,
+      icon: () => <MenuIcon name="book-outline" size={20} color={"#ffffff"} />,
+    },
+    {
+      item: MenuItemScreensRoutes.PROJECTS,
+      icon: () => <MenuIcon name="folder-open-outline" size={20} color={"#ffffff"} />,
+    },
+    {
+      item: MenuItemScreensRoutes.UPLOAD,
+      icon: () => <MenuIcon name="cloud-upload-outline" size={20} color={"#ffffff"} />,
+    },
+    {
+      item: MenuItemScreensRoutes.PREMIUM,
+      icon: () => <MenuIcon name="pricetags-outline" size={20} color={"#ffffff"} />,
+    },
+    {
+      item: MenuItemScreensRoutes.EARNING,
+      icon: () => <MenuIcon name="cash-outline" size={20} color={"#ffffff"} />,
+    },
+    {
+      item: MenuItemScreensRoutes.SERVICES,
+      icon: () => <MenuIcon name="briefcase-outline" size={20} color={"#ffffff"} />,
+    },
+    {
+      item: MenuItemScreensRoutes.ASK,
+      icon: () => <MenuIcon name="chatbubble-ellipses-outline" size={20} color={"#ffffff"} />,
+    },
+    {
+      item: MenuItemScreensRoutes.ESCROW_SITE,
+      icon: () => <MenuIcon name="shield-checkmark-outline" size={20} color={"#ffffff"} />,
+    },
+  ];
 
-    const handleMenuItemPressed = (pressedItem: MenuItemScreensRoutes) => {
-        if (pressedItem === MenuItemScreensRoutes.COURSES) {
-            navigation.navigate('App', { screen: 'Courses' })
-        } else if (pressedItem === MenuItemScreensRoutes.PROJECTS) {
-            navigation.navigate('App', { screen: 'Projects' })
-        } else if (pressedItem === MenuItemScreensRoutes.UPLOAD) {
-            navigation.navigate('App', { screen: 'Upload' })
-        } else if (pressedItem === MenuItemScreensRoutes.PREMIUM) {
-            navigation.navigate('App', { screen: 'Premium' })
-        } else if (pressedItem === MenuItemScreensRoutes.EARNING) {
-            navigation.navigate('App', { screen: 'Earning' })
-        } else if (pressedItem === MenuItemScreensRoutes.SERVICES) {
-            navigation.navigate('App', { screen: 'Services' })
-        } else if (pressedItem === MenuItemScreensRoutes.ASK) {
-            navigation.navigate('App', { screen: 'Ask' })
-        } else if (pressedItem === MenuItemScreensRoutes.ESCROW_SITE) {
-            Linking.openURL('https://www.solvaafrica.com/').catch(error => {
-                Alert.alert('URL Error!', error?.message ?? 'Could not open link')
-            })
-        }
-        return
+  const handleMenuItemPressed = (pressedItem: MenuItemScreensRoutes) => {
+    if (pressedItem === MenuItemScreensRoutes.COURSES) {
+      navigation.navigate("App", { screen: "Courses" });
+    } else if (pressedItem === MenuItemScreensRoutes.PROJECTS) {
+      navigation.navigate("App", { screen: "Projects" });
+    } else if (pressedItem === MenuItemScreensRoutes.UPLOAD) {
+      navigation.navigate("App", { screen: "Upload" });
+    } else if (pressedItem === MenuItemScreensRoutes.PREMIUM) {
+      navigation.navigate("App", { screen: "Premium" });
+    } else if (pressedItem === MenuItemScreensRoutes.EARNING) {
+      navigation.navigate("App", { screen: "Earning" });
+    } else if (pressedItem === MenuItemScreensRoutes.SERVICES) {
+      navigation.navigate("App", { screen: "Services" });
+    } else if (pressedItem === MenuItemScreensRoutes.ASK) {
+      navigation.navigate("App", { screen: "Ask" });
+    } else if (pressedItem === MenuItemScreensRoutes.ESCROW_SITE) {
+      Linking.openURL("https://www.solvaafrica.com/").catch((error) => {
+        Alert.alert("URL Error!", error?.message ?? "Could not open link");
+      });
     }
+    return;
+  };
 
-    useEffect(() => {
-        const getUserSubscriptionStatus = async () => {
-            try {
-                const response = await AUTH_API_CLIENT.get('/sub/status');
-                if (response.status === 200) {
-                    const isSubscribed = response.data.isSubscribed;
-                    useUserSubscriptionStore.setState({ isSubscribed })
-                }
-            } catch (error: any) {
-                if (!error.response) {
-                    Alert.alert('Error', 'Kindly check your internet connection')
-                }
-                console.log('Error fetching user subscription status', error)
-            }
+  useEffect(() => {
+    useUserSubscriptionStore.setState({ isSubscribed });
+    const getUserSubscriptionStatus = async () => {
+      try {
+        const response = await AUTH_API_CLIENT.get("/sub/status");
+        if (response.status === 200) {
+          const userCurrentSubStatus = response.data.isSubscribed;
+          if (!userCurrentSubStatus) {
+            setIsSubscribed(false);
+          }
         }
-
-        const getSlidesImages = async () => {
-            try {
-                const res = await PUB_API_CLIENT.get('/slider')
-                const { data: slidesImages } = res.data;
-
-                if (slidesImages.length) {
-                    setSlidesImages(slidesImages)
-                    return
-                }
-            } catch (error) {
-                console.log('Error getting slides images', error)
-            }
+      } catch (error: any) {
+        if (!error.response) {
+          Alert.alert("Error", "Kindly check your internet connection");
         }
-        getUserSubscriptionStatus();
-        getSlidesImages();
-    }, [])
+        console.log("Error fetching user subscription status", error);
+      }
+    };
 
-    return (
-        <View style={globalStyles.screen}>
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginBottom: hscale(20) }}>
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <AvatarView />
-                    <View style={{ marginLeft: 8 }}>
-                        <Text style={styles.greetUserText}>Hello, {userData.fullName.split(' ')[0]}</Text>
-                        <Text style={[globalStyles.bodyText, { fontSize: mscale(14) }]}>Here's what is happening today.</Text>
-                    </View>
-                </View>
+    const getSlidesImages = async () => {
+      try {
+        const res = await PUB_API_CLIENT.get("/slider");
+        const { data: slidesImages } = res.data;
 
-                {/* notification icon */}
-                <Icon name='bell' color={colors.primary} size={24} />
-            </View>
+        if (slidesImages.length) {
+          setSlidesImages(slidesImages);
+          return;
+        }
+      } catch (error) {
+        console.log("Error getting slides images", error);
+      }
+    };
+    getUserSubscriptionStatus();
+    getSlidesImages();
+  }, []);
 
-            <Carousel
-                height={hscale(100)}
-                showsControls={false}
-                autoplay={true}
-                loop={true}
-                pagingEnabled={true}
-                bounces={true}
-                activeDotStyle={{ backgroundColor: colors.primary }}
-                dotStyle={{ backgroundColor: colors.sliderDotsInactive }}
-                dotsContainerStyle={{ bottom: hscale(-20) }}
-                mergeStyles={true}
-            >
-                {slidesImages.length ? slidesImages.map((currentSlideImage, index) => (
-                    <View style={{ width: width - (screenHorizontalPadding * 2), height: '100%' }} key={index + currentSlideImage}>
-                        <Image source={{ uri: currentSlideImage.url }} style={{ width: '100%', height: '100%' }} />
-                    </View>
-                )) : new Array(3).map((_, index) => (
-                    <View style={{ width: width - (screenHorizontalPadding * 2), height: '100%' }} key={index}>
-                        <Image source={slidesPlaceholder} style={{ width: '100%', height: '100%' }} />
-                    </View>
-                ))}
-            </Carousel>
-
-
-            {/* menu view */}
-            <View style={styles.menuView}>
-                {MENU_ITEMS.map((currentItem, index) => (
-                    <Pressable key={currentItem.item + index} style={styles.menuItemView} onPress={() => handleMenuItemPressed(currentItem.item)}>
-                        <View style={styles.menuItemIconView}>
-                            {currentItem.icon()}
-                        </View>
-                        <Text style={{ textAlign: 'justify', marginTop: 8, fontFamily: 'Inter-Medium', color: colors.bodyText, fontSize: mscale(12) }}>{currentItem.item}</Text>
-                    </Pressable>
-                ))}
-            </View>
+  return (
+    <View style={globalStyles.screen}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginBottom: hscale(20),
+        }}
+      >
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <AvatarView />
+          <View style={{ marginLeft: 8 }}>
+            <Text style={styles.greetUserText}>Hello, {userData.fullName.split(" ")[0]}</Text>
+            <Text style={[globalStyles.bodyText, { fontSize: mscale(14) }]}>
+              Here's what is happening today.
+            </Text>
+          </View>
         </View>
-    )
+
+        {/* notification icon */}
+        <Icon name="bell" color={colors.primary} size={24} />
+      </View>
+
+      <Carousel
+        height={hscale(100)}
+        showsControls={false}
+        autoplay={true}
+        loop={true}
+        pagingEnabled={true}
+        bounces={true}
+        activeDotStyle={{ backgroundColor: colors.primary }}
+        dotStyle={{ backgroundColor: colors.sliderDotsInactive }}
+        dotsContainerStyle={{ bottom: hscale(-20) }}
+        mergeStyles={true}
+      >
+        {slidesImages.length
+          ? slidesImages.map((currentSlideImage, index) => (
+              <View
+                style={{ width: width - screenHorizontalPadding * 2, height: "100%" }}
+                key={index + currentSlideImage}
+              >
+                <Image
+                  source={{ uri: currentSlideImage.url }}
+                  style={{ width: "100%", height: "100%" }}
+                />
+              </View>
+            ))
+          : new Array(3).map((_, index) => (
+              <View
+                style={{ width: width - screenHorizontalPadding * 2, height: "100%" }}
+                key={index}
+              >
+                <Image source={slidesPlaceholder} style={{ width: "100%", height: "100%" }} />
+              </View>
+            ))}
+      </Carousel>
+
+      {/* menu view */}
+      <View style={styles.menuView}>
+        {MENU_ITEMS.map((currentItem, index) => (
+          <Pressable
+            key={currentItem.item + index}
+            style={styles.menuItemView}
+            onPress={() => handleMenuItemPressed(currentItem.item)}
+          >
+            <View style={styles.menuItemIconView}>{currentItem.icon()}</View>
+            <Text
+              style={{
+                textAlign: "justify",
+                marginTop: 8,
+                fontFamily: "Inter-Medium",
+                color: colors.bodyText,
+                fontSize: mscale(12),
+              }}
+            >
+              {currentItem.item}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    greetUserText: { fontFamily: 'Inter-Bold', fontSize: mscale(16), color: colors.black },
-    menuView: {
-        backgroundColor: '#F9F1FE',
-        marginTop: hscale(24),
-        borderRadius: mscale(20),
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignItems: 'stretch',
-        justifyContent: 'center',
-        gap: 8,
-        paddingVertical: hscale(20),
-    },
-    menuItemView: {
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        flex: 1,
-        paddingVertical: hscale(20),
-        height: 'auto',
-        maxWidth: wscale(80),
-        minWidth: wscale(80)
-    },
-    menuItemIconView: {
-        backgroundColor: colors.primary,
-        height: hscale(40),
-        width: wscale(40),
-        borderRadius: '100%',
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  greetUserText: { fontFamily: "Inter-Bold", fontSize: mscale(16), color: colors.black },
+  menuView: {
+    backgroundColor: "#F9F1FE",
+    marginTop: hscale(24),
+    borderRadius: mscale(20),
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "stretch",
+    justifyContent: "center",
+    gap: 8,
+    paddingVertical: hscale(20),
+  },
+  menuItemView: {
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flex: 1,
+    paddingVertical: hscale(20),
+    height: "auto",
+    maxWidth: wscale(80),
+    minWidth: wscale(80),
+  },
+  menuItemIconView: {
+    backgroundColor: colors.primary,
+    height: hscale(40),
+    width: wscale(40),
+    borderRadius: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
