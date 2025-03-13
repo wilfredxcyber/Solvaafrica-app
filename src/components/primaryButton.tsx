5fe6912af4939ac685c78def6fd07836c44fd201
bytegrn <greenbrightsmart@gmail.com>
2025-03-13 22:15:59 +0100

    add activity indicatort to button

--- src/components/primaryButton.tsx
+++ src/components/primaryButton.tsx
@@ -1,24 +1,45 @@
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, StyleSheet, Text, ActivityIndicator } from "react-native";
 import { useState } from "react";
 
 import { hscale, mscale } from "../helpers/metric";
 import { colors } from "../constants/theme";
 
+type TprimaryButtonProps = { text: string; onPress: () => void; isLoading?: boolean };
 
-type TprimaryButtonProps = { text: string, onPress: () => void }
+export default function PrimaryButton({ text, onPress, isLoading = false }: TprimaryButtonProps) {
+  const [pressed, setPressed] = useState(false);
 
-
-export default function PrimaryButton({ text, onPress }: TprimaryButtonProps) {
-    const [pressed, setPressed] = useState(false);
-
-    return (
-        <Pressable style={[styles.buttonView, { opacity: pressed ? 0.9 : 1 }]} onPress={onPress} onPressIn={() => setPressed(true)} onPressOut={() => setPressed(false)}>
-            <Text style={styles.buttonText}>{text}</Text>
-        </Pressable>
-    )
+  return (
+    <Pressable
+      style={[styles.buttonView, { opacity: pressed ? 0.9 : 1 }]}
+      onPress={onPress}
+      onPressIn={() => setPressed(true)}
+      onPressOut={() => setPressed(false)}
+      disabled={isLoading}
+    >
+      {!isLoading ? (
+        <Text style={styles.buttonText}>{text}</Text>
+      ) : (
+        <ActivityIndicator size={"small"} color={"#ffffff"} />
+      )}
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
-    buttonView: { backgroundColor: colors.primary, width: '100%', borderRadius: mscale(100) },
-    buttonText: { paddingVertical: hscale(20), textAlign: 'center', color: '#ffffff', fontFamily: 'Inter-Medium', fontSize: mscale(16), textTransform: 'capitalize' }
-})
+  buttonView: {
+    backgroundColor: colors.primary,
+    width: "100%",
+    borderRadius: mscale(100),
+    minHeight: hscale(60),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#ffffff",
+    fontFamily: "Inter-Medium",
+    fontSize: mscale(16),
+    textTransform: "capitalize",
+  },
+});
