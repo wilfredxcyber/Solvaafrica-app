a34f086dd3edd826d91a66a699cfac25c247eac8
bytegrn <greenbrightsmart@gmail.com>
2025-04-12 09:14:43 +0100

    save app.config.js

--- app.config.js
+++ app.config.js
@@ -1,12 +1,6 @@
 const IS_DEV = process.env.APP_VARIANT === 'development';
 const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
 
-
-export default ({ config }) => ({
-    ...config,
-})
-
-
 const getUniqueIdentifier = () => {
     if (IS_DEV) {
         return 'com.bytegrn.solvaafrica.dev';
@@ -30,3 +24,20 @@ const getAppName = () => {
 
     return 'Solva';
 };
+
+
+export default ({ config }) => ({
+    ...config,
+    name: getAppName(),
+    ios: {
+        ...config.ios,
+        bundleIdentifier: getUniqueIdentifier(),
+    },
+    android: {
+        ...config.android,
+        package: getUniqueIdentifier(),
+    },
+})
+
+
+
