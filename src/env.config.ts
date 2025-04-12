c330dff3d92caa2b0924d4e8309ee0e52e5afb12
bytegrn <greenbrightsmart@gmail.com>
2025-04-12 19:45:44 +0100

    add prod base endpoint

--- src/env.config.ts
+++ src/env.config.ts
@@ -1,4 +1,4 @@
 export const ENV_CONFIG = {
     dev: { 'BASE_API_URL': 'https://solva-backend.onrender.com/api/v1' },
-    prod: { 'BASE_API_URL': 'https://solva-backend.onrender.com' },
+    prod: { 'BASE_API_URL': 'https://api.solvaafrica.com/api/v1' },
 }
