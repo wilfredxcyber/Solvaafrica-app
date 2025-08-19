74667f4cb125ddfeded71ae75def6669f0ebf696
israel-sanmi <ilesanmiisrael@outlook.com>
2025-08-19 17:01:45 +0100

    android version fix

--- app.config.js
+++ app.config.js
@@ -1,43 +1,55 @@
-const IS_DEV = process.env.APP_VARIANT === 'development';
-const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
 
 const getUniqueIdentifier = () => {
-    if (IS_DEV) {
-        return 'com.bytegrn.solvaafrica.dev';
-    }
+  if (IS_DEV) {
+    return "com.bytegrn.solvaafrica.dev";
+  }
 
-    if (IS_PREVIEW) {
-        return 'com.bytegrn.solvaafrica.preview';
-    }
+  if (IS_PREVIEW) {
+    return "com.bytegrn.solvaafrica.preview";
+  }
 
-    return 'com.bytegrn.solvaafrica';
+  return "com.bytegrn.solvaafrica";
 };
 
 const getAppName = () => {
-    if (IS_DEV) {
-        return 'Solva (Dev)';
-    }
+  if (IS_DEV) {
+    return "Solva (Dev)";
+  }
 
-    if (IS_PREVIEW) {
-        return 'Solva (Preview)';
-    }
+  if (IS_PREVIEW) {
+    return "Solva (Preview)";
+  }
 
-    return 'Solva';
+  return "Solva";
 };
 
-
 export default ({ config }) => ({
-    ...config,
-    name: getAppName(),
-    ios: {
-        ...config.ios,
-        bundleIdentifier: getUniqueIdentifier(),
-    },
-    android: {
-        ...config.android,
-        package: getUniqueIdentifier(),
-    },
-})
+  ...config,
+  name: getAppName(),
+  ios: {
+    ...config.ios,
+    bundleIdentifier: getUniqueIdentifier(),
+  },
+  android: {
+    ...config.android,
+    package: getUniqueIdentifier(),
+  },
 
-
-
+  plugins: [
+    [
+      "expo-build-properties",
+      {
+        android: {
+          compileSdkVersion: 35,
+          targetSdkVersion: 35,
+          buildToolsVersion: "35.0.0", 
+        },
+        ios: {
+          deploymentTarget: "15.1",
+        },
+      },
+    ],
+  ],
+});
