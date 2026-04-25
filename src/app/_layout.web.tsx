af7bd348d78f6371ee75a24ef81888facdc53699
lawaltemidayo <lawaltemidayo06@gmail.com>
2026-04-25 21:50:59 +0100

    fix website title

--- src/app/_layout.web.tsx
+++ src/app/_layout.web.tsx
@@ -1,24 +1,29 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import WebAppContainer from "../components/webAppContainer";
+import { Head } from "expo-head";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-  
-      <QueryClientProvider client={queryClient}>
-        <WebAppContainer>
+    <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>Solva Africa</title>
+        <meta property="og:title" content="Solva Africa" />
+        <meta
+          property="og:description"
+          content="Connect with freelancers and opportunities."
+        />
+        <meta property="og:url" content="https://www.solvaafrica.com" />
+      </Head>
+
+      <WebAppContainer>
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen
-            name="(tabs)"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="(auth)"
-          options={{ headerShown: false}}
-          />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         </Stack>
-        </WebAppContainer>
-      </QueryClientProvider>
+      </WebAppContainer>
+    </QueryClientProvider>
   );
 }
